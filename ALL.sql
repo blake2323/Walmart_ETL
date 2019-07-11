89643d7a4afd3355e3b75b18f49a1eef33fb49d0
J Biz Fo Shiz <foshizzlejbizzle@gmail.com>
2019-07-10 18:42:13 -0700

    Concat table created

--- ALL.sql
+++ ALL.sql
@@ -1,8 +1,10 @@
+
 DROP TABLE features;
 DROP TABLE train;
 
 CREATE TABLE features (
-store INT PRIMARY KEY,
+id INT PRIMARY KEY,
+store INT,
 date DATE,
 temperature DECIMAL,
 fuel_price DECIMAL,
@@ -14,7 +16,8 @@ is_holiday BOOLEAN
 SELECT * FROM features;
 
 CREATE TABLE train (
-store INT PRIMARY KEY,
+id INT PRIMARY KEY,
+store INT,
 dept INT,
 date DATE,
 weekly_sales DECIMAL,
@@ -22,3 +25,28 @@ is_holiday BOOLEAN
 );
 
 SELECT * FROM train;
+
+CREATE TABLE combined (
+store INT,
+date DATE,
+temperature DECIMAL,
+fuel_price DECIMAL,
+cpi DECIMAL,
+unemployment DECIMAL,
+is_holiday BOOLEAN,
+weekly_sales DECIMAL,
+PRIMARY KEY(store, date)
+);
+
+SELECT * FROM combined;
+
+SELECT *
+FROM features f
+LEFT JOIN train t
+ON t.id = f.id;
+
+SELECT f.id, f.store, t.dept, f.date, t.weekly_sales,
+f.is_holiday, f.temperature, f.fuel_price, f.cpi, f.unemployment
+FROM features f
+LEFT JOIN train t
+ON t.id = f.id;
